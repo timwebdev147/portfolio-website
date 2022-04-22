23ed5248525482089b0eaddc3b0c884d495caf9c
Akinsanmi Timothy oluwatobi <95864166+timwebdev147@users.noreply.github.com>
2022-04-22 15:27:02 -0500

    Update background.vue

--- src/components/background.vue
+++ src/components/background.vue
@@ -535,7 +535,7 @@ export default {
         width: calc(100% );
         height: 100%;
         /* width: 600px; */
-        border: 1px solid black;
+        /* border: 1px solid black; */
         position: relative;
         display: flex;
         flex-direction: column;
