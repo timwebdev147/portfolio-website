3cd736769d36d7306eb707778ed445d168429e58
timwebdev147 <Timwebdev147@gmail.com>
2022-06-24 13:59:23 +0100

    update

--- src/main.js
+++ src/main.js
@@ -5,12 +5,12 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 import { faB, fas, faCar } from '@fortawesome/free-solid-svg-icons'
 import { far } from '@fortawesome/free-regular-svg-icons'
-import { faFacebook } from '@fortawesome/free-brands-svg-icons'
+// import { faFacebook } from '@fortawesome/free-brands-svg-icons'
 library.add(fas);
 library.add(far)
 library.add(faB)
 library.add(faCar)
-library.add(faFacebook)
+// library.add(faFacebook)
 dom.watch();
 
 createApp(App).component('font-awesome-icon', FontAwesomeIcon)
