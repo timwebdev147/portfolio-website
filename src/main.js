8d33049d2e13a16d1f6a77b5b8955006ecbee152
timwebdev147 <Timwebdev147@gmail.com>
2022-07-17 01:41:14 +0100

    changes in my main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import Vuelidate from 'vuelidate'
 import { dom, library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
@@ -14,7 +13,6 @@ library.add(faCar)
 library.add(faFacebook, faTwitter, faInstagram, faHackerrank, faGithub, faLinkedin)
 dom.watch();
 
-createApp(App).use(Vuelidate);
 
 createApp(App).component('font-awesome-icon', FontAwesomeIcon)
 
